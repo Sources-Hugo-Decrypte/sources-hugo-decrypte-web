fa40f681fd5e72b1eb13a5e3a93c161441e14564
elkoiko <tcouacault@gmail.com>
2022-01-08 23:49:50 +0100

    Install Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Sources Hugo Décrypte</h1>
+    <h1 className="text-4xl font-light text-center my-24">
+      Sources Hugo Décrypte
+    </h1>
   );
 }
 
