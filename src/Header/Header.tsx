827cc007def40e4cd6a06adb9c4d014fc66eb53b
elkoiko <tcouacault@gmail.com>
2022-01-09 18:36:21 +0100

    Add GitHub Mark logo

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import githubMark from './GitHub-Mark.png'
 
 function Header() {
   return (
-    <header className="py-4 px-2 flex justify-between">
-        <img className="inline-block" src="" alt="Sources Hugo Décrypte Logo"/>
-        <nav className="flex justify-around">
-            <a href="#" className="mx-2">Accueil</a>
-            <a href="#" className="mx-2">Recherche</a>
-            <a href="#" className="mx-2">À propos</a>
+    <header className="py-4 px-2 border-b border-gray-200">
+      <div className="flex items-center">
+        <img className="inline-block" src="" alt="Sources Hugo Décrypte Logo" />
+        <nav className="ml-auto">
+          <ul className="flex space-x-6">
+            <li>
+              <a href="#" className="mx-2">Accueil</a>
+            </li>
+            <li>
+              <a href="#" className="mx-2">Recherche</a>
+            </li>
+            <li>
+              <a href="#" className="mx-2">À propos</a>
+            </li>
+          </ul>
         </nav>
+        <div className="border-l border-gray-200 ml-4 pl-4">
+          <a href="https://github.com/Sources-Hugo-Decrypte/sources-hugo-decrypte-web" rel="external nofollow" title="Voir les sources sur GitHub">
+            <img src={githubMark} alt="Github Mark" className="h-6 w-6" />
+          </a>
+        </div>
+      </div>
     </header>
   );
 }
