8c749d95a083b715167904d8c41f2036ea8798ba
elkoiko <tcouacault@gmail.com>
2022-02-06 17:18:38 +0100

    Design mobile version for Remarck component

--- src/Components/Remark/Remark.tsx
+++ src/Components/Remark/Remark.tsx
@@ -1,5 +1,5 @@
 function Remark() {
-    return <p className="w-2/3 mx-auto m-8 border rounded p-4">
+    return <p className="m-4 p-4 md:w-2/3 md:my-8 md:mx-auto border rounded">
         Ce site présente des statistiques sur les sources fournies dans la description de chaque vidéo de la chaîne YouTube d’actualité: <a className="underline" href="https://www.youtube.com/c/HugoD%C3%A9crypte" rel="external nofollow">Hugo Décrypte - Actus du Jour</a>.
         <br />Les données sont mises à jour quotidiennement.
     </p>
