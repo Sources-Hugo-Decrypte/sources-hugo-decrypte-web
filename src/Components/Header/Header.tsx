7af171a2e1c0b1fd4316870a2b7a436b82948f63
elkoiko <tcouacault@gmail.com>
2022-02-06 17:39:02 +0100

    Design mobile version for Header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,12 +4,12 @@ import githubMark from './GitHub-Mark.png'
 function Header() {
   return (
     <header className="p-2 border-b border-gray-200 shadow-sm">
-      <div className="flex items-center">
+      <div className="flex flex-col md:flex-row md:items-center">
         <a href="/">
           <img className="inline-block h-12" src={logo} alt="Sources Hugo Décrypte Logo" />
         </a>
-        <nav className="ml-auto">
-          <ul className="flex space-x-6">
+        <nav className="md:ml-auto">
+          <ul className="flex justify-around mt-2 md:mt-0 md:space-x-6">
             <li>
               <a href="/" className="p-2 rounded-md hover:text-white bg-gradient-to-r hover:from-purple-400 hover:to-blue-400">Accueil</a>
             </li>
@@ -19,9 +19,14 @@ function Header() {
             <li>
               <a href="/" className="p-2 rounded-md hover:text-white bg-gradient-to-r hover:from-purple-400 hover:to-blue-400">À propos</a>
             </li>
+            <li className="md:hidden">
+              <a href="https://github.com/Sources-Hugo-Decrypte/" rel="external nofollow" title="Voir les sources sur GitHub">
+                <img src={githubMark} alt="Github Mark" className="h-6 w-6" />
+              </a>
+            </li>
           </ul>
         </nav>
-        <div className="border-l border-gray-200 ml-4 pl-4">
+        <div className="border-l border-gray-200 hidden md:block md:ml-4 md:pl-4">
           <a href="https://github.com/Sources-Hugo-Decrypte/" rel="external nofollow" title="Voir les sources sur GitHub">
             <img src={githubMark} alt="Github Mark" className="h-6 w-6" />
           </a>
