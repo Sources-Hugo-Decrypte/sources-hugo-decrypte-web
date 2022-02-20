20b996293702e8e0fe723f0d0f6ece6d238d5402
git.youn <git.youn@gmail.com>
2022-02-20 12:42:07 +0100

    parallelized sql requests (home | period)

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,15 +1,17 @@
 import Header from './Header/Header';
 import Remark from './Remark/Remark';
 import Top10 from './Top10/Top10';
-import NbSources from './Period/Period';
+import Period from './Period/Period';
 import Footer from './Footer/Footer';
 import Overview from './Overview/Overview';
 import HomeData from "../Data/HomeData";
+import PeriodData from "../Data/PeriodData";
 import SectionTitle from "./Common/SectionTitle";
 import useFetch from "../Utils/useFetch";
 
 function App() {
   const [homeData, loading] = useFetch<HomeData>('/.netlify/functions/home')
+  const [periodData, loading2] = useFetch<PeriodData>('/.netlify/functions/period')
 
   if (loading) {
     return (<>
@@ -28,7 +30,7 @@ function App() {
       <Remark />
       <Overview {...homeData.overview} />
       <Top10 {...homeData.top10} />
-      <NbSources {...homeData.period} />
+      <Period {...periodData} />
       <Footer />
     </>
   );
--- src/Data/HomeData.ts
+++ src/Data/HomeData.ts
@@ -1,11 +1,9 @@
 import OverviewData from "./OverviewData"
 import Top10Data from "./Top10Data"
-import PeriodData from "./PeriodData"
 
 type HomeData = {
     overview: OverviewData,
-    top10: Top10Data,
-    period: PeriodData
+    top10: Top10Data
 }
 
 export default HomeData
