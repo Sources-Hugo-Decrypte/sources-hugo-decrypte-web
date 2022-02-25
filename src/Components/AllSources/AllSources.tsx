d61156f29d280b05bb958a98e63f024ac46bfd23
git.youn <git.youn@gmail.com>
2022-02-25 21:24:37 +0100

    add arrow

--- src/Components/AllSources/AllSources.tsx
+++ src/Components/AllSources/AllSources.tsx
@@ -1,6 +1,7 @@
 import AllSourcesData from '../../Data/AllSourcesData';
 import useFetch from "../../Utils/useFetch";
 import SectionTitle from "../Common/SectionTitle";
+import "./allsources.css";
 import List from 'list.js';
 
 function AllSources() {    
@@ -33,10 +34,10 @@ function AllSources() {
     <section className="m-4 w-min md:mx-auto md:my-16">
         <div className="shadow-md rounded p-5">
             <div id="sourcesList">
-                <input className="search border-2 rounded-full px-3 mr-2" placeholder="Recherche" />
-                <button className="sort bg-gray-100 hover:bg-gray-200 rounded-full px-4 py-1 mx-2" data-sort="name" data-order="asc">Source</button>
-                <button className="sort bg-gray-100 hover:bg-gray-200 rounded-full px-4 py-1 mx-2" data-sort="nbLinks">Liens</button>
-                <button className="sort bg-gray-100 hover:bg-gray-200 rounded-full px-4 py-1 mx-2" data-sort="lastDate">Date</button>
+                <input className="search border-2 rounded-full px-3 mr-2" type="text" placeholder="Recherche" />
+                <button className="sort bg-gray-100 hover:bg-gray-200 rounded-full px-4 py-1 mx-2" data-sort="name" data-order="asc">Sources</button>
+                <button className="sort bg-gray-100 hover:bg-gray-200 rounded-full px-4 py-1 mx-2" data-sort="grade" data-order="asc">Liens</button>
+                <button className="sort bg-gray-100 hover:bg-gray-200 rounded-full px-4 py-1 mx-2" data-sort="lastDate">Dates</button>
 
                 <div className="mt-4 font-bold grid grid-flow-col auto-cols-min gap-4 p-1">
                     <p className="w-11">Rang</p>
