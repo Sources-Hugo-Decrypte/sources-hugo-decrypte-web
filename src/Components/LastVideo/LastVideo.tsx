40b043cf247b984b39f2077e2cdc2b28ee30a21f
Thomas Couacault <tcouacault@gmail.com>
2022-02-27 23:20:34 +0100

    Implement responsive design

--- src/Components/LastVideo/LastVideo.tsx
+++ src/Components/LastVideo/LastVideo.tsx
@@ -2,27 +2,27 @@ import LastVideoData from "../../Data/LastVideoData";
 import SectionTitle from "../Common/SectionTitle";
 
 function LastVideo(data: LastVideoData) {
-    return <section className="m-4 md:w-2/3 md:mx-auto md:my-16">
+    return <section className="m-4 lg:w-2/3 lg:mx-auto lg:my-16">
         <SectionTitle title="Dernière vidéo" />
-        <div className="shadow-md rounded md:p-5 grid grid-cols-4 gap-4">
-            <img alt="thumbnail" src={data.thumbnail} className="my-auto rounded" />
-            <div className="col-span-3">
-                <a href={data.link} rel="external nofollow" title="Ouvrir dans youtube" className="underline">
-                    <p className="mb-1 text-sm text-gray-700">{data.name}</p>
+        <div className="shadow-md rounded px-3 text-center lg:p-5 lg:grid lg:grid-cols-4 lg:gap-4">
+            <img alt="thumbnail" src={data.thumbnail} className="m-auto rounded" />
+            <div className="lg:col-span-3">
+                <a href={data.link} rel="external nofollow" title="Ouvrir dans youtube" className="inline-block my-4 text-sm text-gray-700 underline">
+                    {data.name}
                 </a>
-                <p className="mb-3 text-sm text-gray-700">
+                <p className="mb-3 text-sm text-gray-700 lg:text-left">
                     Publiée le {new Date(data.date).toLocaleDateString('fr-FR', { day: 'numeric', month: 'short', year: 'numeric' })}
                 </p>
-                <p className="font-bold mb-3 text-sm text-gray-700">
+                <p className="font-bold mb-3 text-sm text-gray-700 lg:text-left">
                     {data.totalSources === 0
                         ? ('Aucune source citée')
                         : (
-                            `${data.totalSources} Source${data.totalSources > 1 && ('s')}
+                            `${data.totalSources} source${data.totalSources > 1 && ('s')}
                             (${data.totalLinks} lien${data.totalLinks > 1 && ('s')})`
                         )
                     }
                 </p>
-                <div className="flex flex-wrap justify-start gap-x-1">
+                <div className="flex flex-wrap justify-evenly md:justify-center lg:justify-start gap-x-1">
                     {data.sources.url.map(link => (
                         <a href={link} rel="external nofollow" title={link} key={link} className="rounded-full bg-gray-200 px-2 py-1 my-1 text-xs">
                             {data.sources.common_name[data.sources.url.indexOf(link)]}
