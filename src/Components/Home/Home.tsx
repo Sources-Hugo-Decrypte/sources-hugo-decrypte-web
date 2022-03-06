1cc29faab17d40de0461d9647df74cd80742bb28
git.youn <git.youn@gmail.com>
2022-03-06 14:06:30 +0100

    update files for automatic merge

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,31 @@
+import Remark from '../Remark/Remark';
+import Top10 from '../Top10/Top10';
+import Overview from '../Overview/Overview';
+import HomeData from "../../Data/HomeData";
+import SectionTitle from "../Common/SectionTitle";
+import useFetch from "../../Utils/useFetch";
+import Period from '../Period/Period';
+
+function Home() {
+  const [homeData, loading] = useFetch<HomeData>('/.netlify/functions/home')
+
+  if (loading) {
+    return (<>
+      <Remark />
+      <div className="h-screen text-center my-20">
+        <SectionTitle title="Chargement..." />
+      </div>
+    </>)
+  }
+
+  return (
+    <>
+      <Remark />
+      <Overview {...homeData.overview} />
+      <Top10 {...homeData.top10} />
+      <Period />
+    </>
+  );
+}
+
+export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
