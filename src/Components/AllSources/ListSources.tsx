89138d27ade1474028de4725ac667317c0316aef
Thomas Couacault <tcouacault@gmail.com>
2022-03-08 23:42:19 +0100

    Display sources data in new CSS grid based table

--- src/Components/AllSources/ListSources.tsx
+++ src/Components/AllSources/ListSources.tsx
@@ -144,103 +144,38 @@ function ListSources({ fetchedData, loading }: { fetchedData: AllSourcesData, lo
                         </div>
                     )}
 
-                {/* <div className="invisible md:visible font-bold grid grid-flow-row md:grid-flow-col md:auto-cols-min md:gap-4 max-h-0 md:max-h-min p-1">
-                    <p>{listLabels.grade}</p>
-                    <p>{listLabels.name}</p>
-                    <p>{listLabels.totalLinks}</p>
-                    <p>{listLabels.percentage}</p>
-                    <p>{listLabels.lastDate}</p>
-                </div> */}
-
-                <table className="grid grid-cols-1 md:grid-cols-5 border-2 border-b-0 rounded">
-                    <thead className="contents">
-                        <tr className="contents">
-                            <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Rang</th>
-                            <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Source</th>
-                            <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Liens</th>
-                            <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Part</th>
-                            <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2">Date</th>
-                        </tr>
-                    </thead>
-                    <tbody className="contents">
-                        <tr className="contents">
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">000001</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">Lani</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">Ovendale</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">lovendale0@w3.org</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2">7850 Old Shore Drive</td>
-                        </tr>
-                        <tr className="contents">
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">000001</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">Lani</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">Ovendale</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">lovendale0@w3.org</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2">7850 Old Shore Drive</td>
-                        </tr>
-                        <tr className="contents">
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">000001</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">Lani</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">Ovendale</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">lovendale0@w3.org</td>
-                            <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2">7850 Old Shore Drive</td>
-                        </tr>
-                    </tbody>
-                </table>
-                {/* {loading
-                    // if loading :
-                    ? (
-                        <span>TODO: Loading placeholder</span>
-                    )
-                    // if not loading :
-                    : (
-                        <ul>
-                            {sourcesData && sourcesData.length > 0 ? (
-                                sourcesData.map(source => (
-                                    <li key={source.name + "-li"}>
-                                        <div ref={refDiv} className="border rounded mb-1 p-1 hover:bg-gray-50" key={source.name + "-div"}>
-                                            <div className="grid grid-flow-col auto-cols-min w-min">
-                                                <p className="w-14 font-bold whitespace-pre">{listLabels.grade}</p>
-                                                <p className="font-bold whitespace-pre"> : </p>
-                                                <p key={source.name + "-grade-" + source.grade}>
-                                                    {source.grade}
-                                                </p>
-                                            </div>
-                                            <div className="grid grid-flow-col auto-cols-min w-min">
-                                                <p className="w-14 font-bold whitespace-pre">{listLabels.name}</p>
-                                                <p className="font-bold whitespace-pre"> : </p>
-                                                <p className="md:break-words" key={source.name + "-name"}>
-                                                    {source.name}
-                                                </p>
-                                            </div>
-                                            <div className="grid grid-flow-col auto-cols-min w-min">
-                                                <p className="w-14 font-bold whitespace-pre">{listLabels.totalLinks}</p>
-                                                <p className="font-bold whitespace-pre"> : </p>
-                                                <p key={source.name + "-totalLinks-" + source.totalLinks}>
-                                                    {source.totalLinks}
-                                                </p>
-                                            </div>
-                                            <div className="grid grid-flow-col auto-cols-min w-min">
-                                                <p className="w-14 font-bold whitespace-pre">{listLabels.percentage}</p>
-                                                <p className="font-bold whitespace-pre"> : </p>
-                                                <p className="whitespace-pre" key={source.name + "-percentage-" + source.percentage}>
-                                                    {source.percentage} %
-                                                </p>
-                                            </div>
-                                            <div className="grid grid-flow-col auto-cols-min w-min">
-                                                <p className="w-14 font-bold whitespace-pre">{listLabels.lastDate}</p>
-                                                <p className="font-bold whitespace-pre"> : </p>
-                                                <p key={source.name + "-lastDate-" + source.lastDate}>
-                                                    {new Date(source.lastDate).toLocaleDateString('fr-FR')}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                ))
-                            ) : (
-                                <div className="text-center border rounded mb-1 p-1">Pas de résultat</div>
-                            )}
-                        </ul>
-                    )} */}
+                {loading ? (<span>TODO: Loading placeholder</span>) : (
+                    <>
+                        {sourcesData && sourcesData.length > 0 ? (
+                            <table className="grid grid-cols-1 md:grid-cols-5 border-2 border-b-0 rounded">
+                                <thead className="contents">
+                                    <tr className="contents">
+                                        <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Rang</th>
+                                        <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Source</th>
+                                        <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Liens</th>
+                                        <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-r-2 border-b-2">Part</th>
+                                        <th className="p-3.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2">Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="contents">
+                                    {
+                                        sourcesData.map(source => (
+                                            <tr className="contents" key={source.name + '-row'}>
+                                                <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">{source.grade}</td>
+                                                <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">{source.name}</td>
+                                                <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">{source.totalLinks}</td>
+                                                <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2 border-r-2">{source.percentage} %</td>
+                                                <td className="p-3.5 pt-2.5 pb-2.5 overflow-hidden whitespace-nowrap text-ellipsis border-b-2">{new Date(source.lastDate).toLocaleDateString('fr-FR')}</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        ) : (
+                            <div className="text-center border rounded mb-1 p-1">Pas de résultat</div>
+                        )}
+                    </>
+                )}
             </div>
         </div>
     )
