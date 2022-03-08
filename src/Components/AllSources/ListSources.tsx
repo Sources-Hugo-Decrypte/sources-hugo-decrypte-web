ece864243114cd7381a2f60f8e09007ba5f72919
Thomas Couacault <tcouacault@gmail.com>
2022-03-08 19:00:30 +0100

    Get rid of useWindowDimensions hook

--- src/Components/AllSources/ListSources.tsx
+++ src/Components/AllSources/ListSources.tsx
@@ -1,7 +1,6 @@
 import { useState, useMemo, useEffect, useRef } from 'react';
 import AllSourcesData from '../../Data/AllSourcesData';
 import AllSourcesSingleData from '../../Data/AllSourcesSingleData';
-import useWindowDimensions from './useWindowDimensions';
 
 enum SortDirection {
     ASCENDING = 'asc',
@@ -86,7 +85,6 @@ function ListSources({ fetchedData, loading }: { fetchedData: AllSourcesData, lo
         setSortConfig(sortConfig => sortConfig = { key, direction })
     }
 
-
     // ---------------- Searching ---------------- //
 
     const filter = (e: { target: { value: any; }; }) => {
@@ -127,75 +125,6 @@ function ListSources({ fetchedData, loading }: { fetchedData: AllSourcesData, lo
 
     const refDiv = useRef<HTMLHeadingElement>(null); // -> ref to the div that contains one source data
 
-    const { windowWidth } = useWindowDimensions();
-
-    // Default width for every element :
-    const sourcesWidthDefault = {
-        grade: 44,
-        name: 0,
-        totalLinks: 48,
-        percentage: 80,
-        lastDate: 82
-    }
-    // Adapt size on screen width :
-    sourcesWidthDefault.name = windowWidth * 2 / 3 - (sourcesWidthDefault.grade + sourcesWidthDefault.totalLinks + sourcesWidthDefault.percentage + sourcesWidthDefault.lastDate + 16 * 4 + 2 * 4 + 16 * 2 + 14);
-    // '+16*4' because there is a gap-4 between each element | '+2*4' because of p-1 for grid div
-
-    // Create style dictionary :
-    const sourcesStyleDefault = {
-        grade: { width: `${sourcesWidthDefault.grade}px` },
-        name: { width: `${sourcesWidthDefault.name}px` },
-        totalLinks: { width: `${sourcesWidthDefault.totalLinks}px` },
-        percentage: { width: `${sourcesWidthDefault.percentage}px` },
-        lastDate: { width: `${sourcesWidthDefault.lastDate}px` }
-    }
-
-    const [sourcesStyle, setSourcesStyle] = useState(sourcesStyleDefault);
-    const [sourcesWidth, setSourcesWidth] = useState(sourcesWidthDefault);
-
-    const currentSourcesWidthDefault = sourcesWidth.grade + sourcesWidth.name + sourcesWidth.totalLinks + sourcesWidth.percentage + sourcesWidth.lastDate + 16 * 4 + 2 * 4 + 2;
-    // '+16*4' because there is a gap-4 between each element | '+2*4' because of p-1 for grid div
-
-    const [currentSourcesWidth, setCurrentSourcesWidth] = useState(currentSourcesWidthDefault);
-
-    // auto-update sourcesStyle and currentSourcesWidth :
-    useEffect(() => {
-        setSourcesStyle(styles => styles = {
-            grade: { width: `${sourcesWidth.grade}px` },
-            name: { width: `${sourcesWidth.name}px` },
-            totalLinks: { width: `${sourcesWidth.totalLinks}px` },
-            percentage: { width: `${sourcesWidth.percentage}px` },
-            lastDate: { width: `${sourcesWidth.lastDate}px` }
-        });
-        setCurrentSourcesWidth(w => w = sourcesWidth.grade + sourcesWidth.name + sourcesWidth.totalLinks + sourcesWidth.percentage + sourcesWidth.lastDate + 16 * 4 + 2 * 4 + 2);
-    }, [sourcesWidth])
-
-    // update layout when resizing :
-    useEffect(() => {
-        window.addEventListener("resize", updateSourcesNamesWidth);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    const updateSourcesNamesWidth = () => {
-        const newSourcesWidth = {
-            grade: sourcesWidth.grade,
-            name: sourcesWidth.name,
-            totalLinks: sourcesWidth.totalLinks,
-            percentage: sourcesWidth.percentage,
-            lastDate: sourcesWidth.lastDate
-        }
-        // Reduce size :
-        if (refDiv.current != null && currentSourcesWidth > refDiv.current.offsetWidth) {
-            newSourcesWidth.name = sourcesWidth.name - (currentSourcesWidth - refDiv.current.offsetWidth);
-        }
-        // Increase size :
-        if (refDiv.current != null && currentSourcesWidth < refDiv.current.offsetWidth) {
-            newSourcesWidth.name = sourcesWidth.name + (refDiv.current.offsetWidth - currentSourcesWidth);
-        }
-        setSourcesWidth(w => w = newSourcesWidth);
-    }
-
-
     // ---------------- Layout ---------------- //
 
     return (
@@ -216,11 +145,11 @@ function ListSources({ fetchedData, loading }: { fetchedData: AllSourcesData, lo
                     )}
 
                 <div className="invisible md:visible font-bold grid grid-flow-row md:grid-flow-col md:auto-cols-min md:gap-4 max-h-0 md:max-h-min p-1">
-                    <p style={sourcesStyle.grade}>{listLabels.grade}</p>
-                    <p style={sourcesStyle.name}>{listLabels.name}</p>
-                    <p style={sourcesStyle.totalLinks}>{listLabels.totalLinks}</p>
-                    <p style={sourcesStyle.percentage}>{listLabels.percentage}</p>
-                    <p style={sourcesStyle.lastDate}>{listLabels.lastDate}</p>
+                    <p>{listLabels.grade}</p>
+                    <p>{listLabels.name}</p>
+                    <p>{listLabels.totalLinks}</p>
+                    <p>{listLabels.percentage}</p>
+                    <p>{listLabels.lastDate}</p>
                 </div>
 
                 {loading
@@ -233,29 +162,27 @@ function ListSources({ fetchedData, loading }: { fetchedData: AllSourcesData, lo
                                         <div className="grid grid-flow-col auto-cols-min w-min">
                                             <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.grade}</p>
                                             <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                            <div style={sourcesStyle.grade} className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-grade-"}></div>
+                                            <div className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-grade-"}></div>
                                         </div>
                                         <div className="grid grid-flow-col auto-cols-min w-min">
                                             <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.name}</p>
                                             <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                            {windowWidth <= 768
-                                                ? (<div className="w-44 h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded"></div>)
-                                                : (<div style={sourcesStyle.name} className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-name"}></div>)}
+                                            <div className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-name"}></div>
                                         </div>
                                         <div className="grid grid-flow-col auto-cols-min w-min">
                                             <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.totalLinks}</p>
                                             <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                            <div style={sourcesStyle.totalLinks} className=" h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-totalLinks-"}></div>
+                                            <div className=" h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-totalLinks-"}></div>
                                         </div>
                                         <div className="grid grid-flow-col auto-cols-min w-min">
                                             <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.percentage}</p>
                                             <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                            <div style={sourcesStyle.percentage} className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-percentage-"}></div>
+                                            <div className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-percentage-"}></div>
                                         </div>
                                         <div className="grid grid-flow-col auto-cols-min w-min">
                                             <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.lastDate}</p>
                                             <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                            <div style={sourcesStyle.lastDate} className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-lastDate-"}></div>
+                                            <div className="h-2 mt-2 ml-1 md:ml-0 bg-gray-300 rounded" key={index + "-lastDate-"}></div>
                                         </div>
                                     </div>
                                 </li>
@@ -272,35 +199,35 @@ function ListSources({ fetchedData, loading }: { fetchedData: AllSourcesData, lo
                                             <div className="grid grid-flow-col auto-cols-min w-min">
                                                 <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.grade}</p>
                                                 <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                                <p style={sourcesStyle.grade} key={source.name + "-grade-" + source.grade}>
+                                                <p key={source.name + "-grade-" + source.grade}>
                                                     {source.grade}
                                                 </p>
                                             </div>
                                             <div className="grid grid-flow-col auto-cols-min w-min">
                                                 <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.name}</p>
                                                 <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                                <p style={sourcesStyle.name} className="md:break-words" key={source.name + "-name"}>
+                                                <p className="md:break-words" key={source.name + "-name"}>
                                                     {source.name}
                                                 </p>
                                             </div>
                                             <div className="grid grid-flow-col auto-cols-min w-min">
                                                 <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.totalLinks}</p>
                                                 <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                                <p style={sourcesStyle.totalLinks} key={source.name + "-totalLinks-" + source.totalLinks}>
+                                                <p key={source.name + "-totalLinks-" + source.totalLinks}>
                                                     {source.totalLinks}
                                                 </p>
                                             </div>
                                             <div className="grid grid-flow-col auto-cols-min w-min">
                                                 <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.percentage}</p>
                                                 <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                                <p style={sourcesStyle.percentage} className="whitespace-pre" key={source.name + "-percentage-" + source.percentage}>
+                                                <p className="whitespace-pre" key={source.name + "-percentage-" + source.percentage}>
                                                     {source.percentage} %
                                                 </p>
                                             </div>
                                             <div className="grid grid-flow-col auto-cols-min w-min">
                                                 <p className="w-14 font-bold whitespace-pre md:invisible md:max-w-0">{listLabels.lastDate}</p>
                                                 <p className="font-bold whitespace-pre md:invisible md:max-w-0"> : </p>
-                                                <p style={sourcesStyle.lastDate} key={source.name + "-lastDate-" + source.lastDate}>
+                                                <p key={source.name + "-lastDate-" + source.lastDate}>
                                                     {new Date(source.lastDate).toLocaleDateString('fr-FR')}
                                                 </p>
                                             </div>
