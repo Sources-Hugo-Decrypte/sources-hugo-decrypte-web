fab8e481f4ed1d82fcab703a905fb02ce3d8a8a3
younrx <git.youn@gmail.com>
2023-06-27 22:29:09 +0200

    update warning banner

--- src/Components/WarningBanner/WarningBanner.tsx
+++ src/Components/WarningBanner/WarningBanner.tsx
@@ -1,6 +1,7 @@
 function WarningBanner() {
-    return <div className="text-center w-screen py-2 bg-[#F7D456]">
+    return <div className="flex flex-col text-center w-screen p-3 bg-[#F7D456]">
         <p>Ce site n'est plus mis à jour. Les données restent toutefois consultables.</p>
+        <p className="text-sm">En raison des limitations de la base de données utilisée comme archive, veuillez réactualiser la page si certaines informations ne s'affichent pas correctement.</p>
     </div>
 }
 
